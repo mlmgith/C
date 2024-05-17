aa2c1c739b6be220b43852bb8ac3c9fcf7336a69
mlmgith <mlmgithub@outlook.fr>
2024-05-17 12:39:00 +0200

    Create exercice3.c

---
+++ exercice3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Déclaration de la variable distance
+    double distance = 123.456;  // Vous pouvez remplacer 123.456 par n'importe quelle autre valeur double
+
+    // Affichage de la variable distance
+    printf("La distance est : %.3f\n", distance);
+
+    return 0;
+}
