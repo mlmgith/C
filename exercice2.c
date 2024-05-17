fb90f2769b8210eff35c5490dca99d8f9f377564
mlmgith <mlmgithub@outlook.fr>
2024-05-17 12:38:01 +0200

    Create exercice2.c

---
+++ exercice2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Déclaration de la variable temperature
+    float temperature = 36.6;  // Vous pouvez remplacer 36.6 par n'importe quelle autre valeur flottante
+
+    // Affichage de la variable temperature
+    printf("La température est : %.1f\n", temperature);
+
+    return 0;
+}
